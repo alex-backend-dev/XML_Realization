3d6c36b8174cb28999bf2713c384c8b3be668dca
Alexander Ulyanitsky <AlexanderUlyanitsky@coherentsolutions.com>
2021-08-31 12:06:01 +0300

    Changes

--- XML/Program.cs
+++ XML/Program.cs
@@ -1,41 +1,10 @@
-﻿using System.IO;
-using System.Linq;
-using Xml;
-
-namespace XML
+﻿namespace XML
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var filename = "Logins.xml";
-
-            XmlHelper xmlHelper = new XmlHelper();
-
-            var document = xmlHelper.GetFile(filename);
-
-            var logins = xmlHelper.GetLogins(document).ToList();
-            using (StreamWriter writetext = new StreamWriter("write.txt")) 
-            {
-                foreach (var loginReading in logins)
-                {
-                    writetext.WriteLine(loginReading);
-                }
-            }
-
-            var correctLogins = xmlHelper.GetInCorrectLogins(logins);
-            using (StreamWriter writetext = new StreamWriter("Incorrect.txt"))
-            {
-                foreach (var incorrectLogin in correctLogins)
-                {
-                    writetext.WriteLine(incorrectLogin);
-                }
-            }
-
-
-            JsonReading jsonReading = new JsonReading();
-
-            jsonReading.MigrateToJson(logins);
+            
         }
     }
 }
---
+++ XML/Xml Helper Entities/LoginManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xml;
+
+namespace XML
+{
+    /// <summary>
+    /// Wrapper for writing correctLogins and incorrectLogins
+    /// </summary>
+    public class LoginManager
+    {
+        private string filename;
+
+        public LoginManager(string filename)
+        {
+            this.filename = filename;
+        }
+
+        /// <summary>
+        /// Writing convert Xml in right format into file
+        /// </summary>
+        /// <param name="xmlHelper"></param>
+        public void CorrectLogin(XmlHelper xmlHelper)
+        {
+            var document = xmlHelper.GetFile(filename);
+
+            var logins = xmlHelper.GetLogins(document).ToList();
+            using (StreamWriter writetext = new StreamWriter("write.txt"))
+            {
+                foreach (var loginReading in logins)
+                {
+                    writetext.WriteLine(loginReading);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Writing incorrect logins into file
+        /// </summary>
+        /// <param name="logins"></param>
+        /// <param name="xmlHelper"></param>
+        public void InCorrectLogin(IEnumerable<Login> logins, XmlHelper xmlHelper)
+        {
+            var correctLogins = xmlHelper.GetInCorrectLogins(logins);
+            using (StreamWriter writetext = new StreamWriter("Incorrect.txt"))
+            {
+                foreach (var incorrectLogin in correctLogins)
+                {
+                    writetext.WriteLine(incorrectLogin);
+                }
+            }
+        }
+    }
+}
